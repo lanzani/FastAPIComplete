ae9be1c2e047ca7191eee9c3512611ef007f3d80
federico <federico.finder@gmail.com>
2022-04-05 23:35:28 +0200

    :card_file_box: Fixed alembic imports.

--- alembic/env.py
+++ alembic/env.py
@@ -5,8 +5,8 @@ from sqlalchemy import pool
 
 from alembic import context
 
-from app import Base
-from app import settings
+from app.database import Base
+from app.config import settings
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
